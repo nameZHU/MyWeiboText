c984584f3a47f090dba5f0ae442aa511c8e5b0d7
zxlfcl <zxifcl@zxlfcldeMacBook-Pro.local>
2015-08-20 10:40:14 +0800

    zhuxiaolei

---
+++ MyTextWeibo/MyTextWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTextWeibo
+//
+//  Created by zxlfcl on 15/8/20.
+//  Copyright (c) 2015年 XiaoLeiZhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTextWeibo/MyTextWeibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTextWeibo
+//
+//  Created by zxlfcl on 15/8/20.
+//  Copyright (c) 2015年 XiaoLeiZhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
